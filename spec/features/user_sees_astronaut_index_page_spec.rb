3b99fbcb7fea7de75347911d0f16cedaca6b8c28
vjp888 <rellik888@gmail.com>
2019-02-01 10:57:58 -0700

    Adds test for mission statistics in astronaut index page

--- spec/features/user_sees_astronaut_index_page_spec.rb
+++ spec/features/user_sees_astronaut_index_page_spec.rb
@@ -27,4 +27,22 @@ RSpec.describe 'As a visitor', type: :feature do
 
     expect(page).to have_content("Average age: 37")
   end
+
+  it 'shows missions for each astronaut in alphabetical order' do
+    astro_1 = Astronaut.create(name: "Steve", age: 23, job: 'engineer')
+    astro_1.missions.create(title: "Apollo 13", time_in_space: 10)
+    astro_1.missions.create(title: "Capricorn 4", time_in_space: 10)
+    astro_1.missions.create(title: "Gemini 7", time_in_space: 10)
+
+    visit astronauts_path
+
+    within '.mission' do
+      expect(page).to have_content("Apollo 13")
+      expect(page).to have_content("Capricorn 4")
+      expect(page).to have_content("Capricorn 7")
+    end
+
+
+
+  end
 end
